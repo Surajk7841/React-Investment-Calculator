0e0f583a6083fa6757e8ba8f28d0865cba8e5a66
Surajk7841 <kotulesuraj@gamil.com>
2023-09-26 14:17:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Form from "./Components/Form";
+import Header from "./Components/Header";
+import Result from "./Components/Result";
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+  const yearlyData = [];
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <Form onCalculate={calculateHandler} />
+      {!userInput && (
+        <p style={{ textAlign: "center" }}>No Investment Calculated Yet.</p>
+      )}
+      {userInput && (
+        <Result
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
